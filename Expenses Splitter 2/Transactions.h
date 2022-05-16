f128204f5ea439b7456da8cd16d0cad778d54cf2
jmacuga <julia.macuga@gmail.com>
2022-05-17 00:07:01 +0200

    transaction categories

--- Expenses Splitter 2/Transactions.h
+++ Expenses Splitter 2/Transactions.h
@@ -1,27 +1,31 @@
+#pragma once
 #include <string>
 #include "Person.h"
 
-
+enum categories { food, alcohol, tabacco, other };
 class Transaction
 {
-    protected:
+protected:
     double money;
-    std::string category;
+    int category;
     Person payer;
-    public:
+public:
+    Transaction(double m, Person p, categories c) : money(m), category(c), payer(p) {};
     virtual void settle() const = 0;
 
 };
 
-class CollectiveTransaction: Transaction
+class CollectiveTransaction : Transaction
 {
-    private:
+private:
 
-    public:
+public:
+    CollectiveTransaction(double m, Person p, categories c) : Transaction(m, p, c) {};
+    void settle() const {};
 };
 
-class SpecificTransaction: Transaction
+class SpecificTransaction : Transaction
 {
-    private:
-    public:
+private:
+public:
 };
--- Expenses Splitter 2/logic_tests.cpp
+++ Expenses Splitter 2/logic_tests.cpp
@@ -1,7 +1,12 @@
-#include "Person.h"
-#include "Trip.h"
+#include <iostream>
+#include "Transactions.h"
 
 int main()
 {
+    Person Milosz(1, "Milosz");
+    double price = 11.5;
+    categories c = food;
+    CollectiveTransaction trans1(price, Milosz, food);
+    std::cout << "\nEnd of logic tests";
     return 0;
 }
