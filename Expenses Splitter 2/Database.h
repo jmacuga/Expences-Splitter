6163119268eb7d3fccf89100ebf1fac7843aa5d4
jmacuga <julia.macuga@gmail.com>
2022-05-18 13:38:08 +0200

    Database operators deleted

--- Expenses Splitter 2/Database.h
+++ Expenses Splitter 2/Database.h
@@ -2,20 +2,10 @@
 #include "Transactions.h"
 #include <vector>
 
-template <typename T> class Database
+template < typename T> class Database
 {
 private:
-    std::vector<T> collection;
 public:
     void save_into_file(std::string path) const;
     void load_from_file(std::string path) const;
-    void operator+=(T elem)
-    {
-        collection.push_back(elem);
-    };
-    T& operator[](int ind)
-    {
-        return collection[ind];
-    };
-
 };
