acaedecf69a4108fca62254faa484ca7b2734d0a
mkowale2 <01168891@pw.edu.pl>
2022-06-04 01:30:50 +0200

    .

--- Expenses Splitter 2/ui_actions.h
+++ Expenses Splitter 2/ui_actions.h
@@ -12,3 +12,5 @@ void add_singular_transaction();
 void settle(Trip& trip);
 void add_collective_transaction(Trip &trip_to_init);
 void add_specific_transaction(Trip &trip_to_init);
+void set_atts_action(Trip &trip_to_init, Person &person_to_add);
+void set_attributes(Trip &trip_to_init, Person &person_to_add);
